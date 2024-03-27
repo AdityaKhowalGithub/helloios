10710642af73d8e6daf336fb93ece6ca35b81b6b
Aditya Khowal <adityakh2003@outlook.com>
2024-03-27 12:45:02 -0700

    Initial Commit

---
+++ Hello,iOS!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello,iOS!
+//
+//  Created by Aditya Khowal on 3/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
